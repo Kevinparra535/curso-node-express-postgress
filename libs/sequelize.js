04c3766fc025b1fe86d074c314d6cdb9031a7372
Kevin Parra <kevinparra535@gmail.com>
2022-03-30 20:42:32 -0500

    [ADD] Configurando y corriendo migraciones con npm scripts

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -5,15 +5,15 @@ const setupModels = require('../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'mysql', // Esta variable nos dice que tipo de base de datos estamos utilizando
+  dialect: 'postgres', // Esta variable nos dice que tipo de base de datos estamos utilizando
   logging: true, // Resulta de comando directo SQL, nos lo va a mostrar en la consola
 });
 
 setupModels(sequelize); // Le pasamos la conexion
 
-sequelize.sync(); // Utiliza los modelos y crea esa estructura, Sincronizando con la base de datos
+// sequelize.sync(); // Utiliza los modelos y crea esa estructura, Sincronizando con la base de datos
 
 module.exports = sequelize;
